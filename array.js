06b586c023703a8da290a11f2ca550828840618e
7dhaval <dhavalsinhsonasaniya7318@gmail.com>
2022-12-31 18:48:25 +0530

    array

---
+++ array.js
@@ -0,0 +1,37 @@
+//array to find large array last element
+
+// var frds = ['vindo','dhaval'];
+
+// console.log(frds[frds.length - 1]);
+
+
+//find the loop to display all data in array
+
+// var frds = ['vindo','ran','dhaval'];
+
+// for(var i=0; i < frds.length; i++){
+//     console.log(frds[i]);
+// }
+
+//ES6 we have for..in and for..off loop too 
+// var frds = ['vindo','dhaval','zeel'];
+
+// for(let elements in frds){  //this will return index number of array
+//     console.log(elements);
+// }
+
+// for(let elements of frds){ //this will return array element
+//     console.log(elements);
+// }
+
+// frds.forEach(function(element,index,array){
+//         console.log(element);
+// });
+
+
+//push and sort
+//  
+
+//u can use unshift() method to adds one or more elements to the begining of an array the new length of an array
+//pop() to remove the last  element of an array
+//shift() to remove first element of an array
