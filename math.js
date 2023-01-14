d665d042d151e8c42df4127301476681e6ace364
7dhaval <dhavalsinhsonasaniya7318@gmail.com>
2023-01-14 21:28:40 +0530

    add math js

---
+++ math.js
@@ -0,0 +1,62 @@
+//Math Object in javascript
+//the javascript math object allow you to perform methemeticals tasks on numbers
+
+//console.log(Math.PI);
+
+//Math.round()
+//returns the value of x rounded to its nearest integer
+
+// let num = 10.2322
+// console.log(Math.round(num));
+
+//Math.pow()
+//Math.pow(x, y) returns the value of x to the power of y
+
+// console.log(Math.pow(2,3));
+
+//Math.sqrt()
+//Math.sqrt(x) returns the square root of x
+
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt(81));
+// console.log(Math.sqrt(66));
+
+// Math.abs()
+// Math.abs(x) returns the absolute (positive) value of x
+
+// console.log(Math.abs(-44));
+
+//Math.ceil()
+// this will returns the nearest integer 
+
+// console.log(Math.ceil(4.50));
+
+//Math.floor()
+//Math.floor(x) returns the value of x rounded down its nearest integer
+
+// console.log(Math.floor(4.7));
+
+//Math.min()
+//Math.min() can be used to find the lowest value in a list of arguments
+//Math.max()
+// console.log(Math.max(23.23,2342,242422,4242424242));
+// console.log(Math.min(0,121,1244,4345,3453,353,-7));
+
+//Math.random()
+//Math.random() returns a random number between 0  to 1 
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10));
+
+//Math.trunc()
+//the trunc() method returs the integer part of a number
+
+// console.log(Math.trunc(4.6));
+// console.log(Math.trunc(-99.1));
+
+
+//if the argumeny is a postive number then Math.trunc() is equivalent to Math.floor(), otherwise Math.trunc() is equivalent to Math.ceil()
+
+
+
